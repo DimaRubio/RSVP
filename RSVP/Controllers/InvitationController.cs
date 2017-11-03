b1ff1a1033e9e8dbdba958cf733de67bb571c1f5
Dima <Dima>
2017-11-03 17:51:21 +0200

    add form, model, validation

---
+++ RSVP/Controllers/InvitationController.cs
@@ -0,0 +1,29 @@
+﻿using RSVP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RSVP.Controllers
+{
+    public class InvitationController : Controller
+    {
+        // GET: Invitation
+        [HttpGet]
+        public ActionResult Invitation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Invitation(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+                return View("Thanks", guest);
+            else
+                // Обнаружена ошибка проверки достоверности
+                return View();
+        }
+    }
+}
